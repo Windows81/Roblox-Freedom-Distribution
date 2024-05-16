b994c50c3e3856fa08aa3a0b3077436d5dd630f1
Windows81 <nastyahmede@gmail.com>
2024-05-16 07:40:39 +0000

    2024-05-16T0740Z

--- Source/util/const.py
+++ Source/util/const.py
@@ -1,4 +1,4 @@
-GIT_RELEASE_VERSION = '''2024-05-15T0905Z'''
+GIT_RELEASE_VERSION = '''2024-05-16T0740Z'''
 GIT_LINK_FORMAT = 'https://github.com/Windows81/Roblox-Filtering-Disabled/releases/download/%s/%s.%s.7z'
 
 DEFAULT_PLACE_ID = 1818
