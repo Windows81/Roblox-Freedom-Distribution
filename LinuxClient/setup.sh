5566e7c8d84b0d7976d50fbe360c7f590aef1edc
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-05-17 11:16:48 +0300

    Update setup.sh

--- LinuxClient/setup.sh
+++ LinuxClient/setup.sh
@@ -1,1 +1,7 @@
-placeholder
+#Version 1.00
+echo "Make sure you have git and wine installed!"
+cd /home/$USER
+mkdir RobloxFreedomDistribution
+wget https://github.com/Windows81/Roblox-Freedom-Distribution/releases/download/2024-05-16T0741Z/RFD.exe -O /home/$USER/RobloxFreedomDistribution
+
+
