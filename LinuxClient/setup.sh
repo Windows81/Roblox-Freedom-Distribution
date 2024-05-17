744a46b392f903ed4cf7a25e736c805f27cee92c
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-05-17 11:52:40 +0300

    Update setup.sh

--- LinuxClient/setup.sh
+++ LinuxClient/setup.sh
@@ -1,6 +1,7 @@
 #Version 1.00
-echo "Make sure you have wget,git and wine installed!"
+echo "Make sure you have wget and wine installed!"
 mkdir /home/$USER/RobloxFreedomDistribution
 mkdir /home/$USER/RobloxFreedomDistribution/settings
+wget https://raw.githubusercontent.com/Windows81/Roblox-Freedom-Distribution/main/LinuxClient/joinnew.sh -O /home/$USER/RobloxFreedomDistribution
 wget https://raw.githubusercontent.com/Windows81/Roblox-Freedom-Distribution/main/LinuxClient/winebin.txt -O /home/$USER/RobloxFreedomDistribution/settings/winebin.txt
 wget https://github.com/Windows81/Roblox-Freedom-Distribution/releases/download/2024-05-16T0741Z/RFD.exe -O /home/$USER/RobloxFreedomDistribution/RFD.exe
