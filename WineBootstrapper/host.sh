f2b9ced35235a0ef3cc473f0a71aab08cc4c90b6
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-06-19 15:15:24 +0300

    Update host.sh

--- WineBootstrapper/host.sh
+++ WineBootstrapper/host.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-winebin=$(cat /home/"$USER"/RobloxFreedomDistribution/settings/winebin.txt)
+winebin=$(cat settings/winebin.txt)
 if ! [ -x "$(command -v "$winebin")" ]; then
     echo 'Error: the command linked in winebin.txt is invalid.' >&2
     exit 1
 fi
 
-command="$winebin /home/\"$USER\"/RobloxFreedomDistribution/RFD.exe server"
+#Add "start" after $winebin if you want the terminal window to disappear 
+command="$winebin RFD.exe server"
 eval "$command"
