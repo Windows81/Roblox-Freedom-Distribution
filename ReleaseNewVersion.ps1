f1a369b44112fff18f6681c58b2d2b605b5d8b00
Windows81 <nastyahmede@gmail.com>
2024-08-20 19:51:35 +0000

    2024-08-21T0251Z

--- ReleaseNewVersion.ps1
+++ ReleaseNewVersion.ps1
@@ -21,7 +21,8 @@ function CreateBinary() {
 		--distpath "$root/Binaries" `
 		--icon "$root/Source/Icon.ico" `
 		--specpath "$root/PyInstallerWork/Spec" `
-		--exclude-module "tqdm.std"
+		--exclude-module "tqdm.std" `
+		--exclude-module "tqdm.gui"
 	foreach ($file in (Get-ChildItem "$root/Binaries/*")) {
 		$files.Add($file)
 	}
