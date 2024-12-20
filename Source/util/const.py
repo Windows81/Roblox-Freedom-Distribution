41295e22ef7b516558e87ab0b051291d0a7ee522
Windows81 <nastyahmede@gmail.com>
2024-12-20 21:38:43 +0000

    2024-12-21T0538Z

--- Source/util/const.py
+++ Source/util/const.py
@@ -1,6 +1,6 @@
-GIT_RELEASE_VERSION = '''0.53.8'''
+GIT_RELEASE_VERSION = '''0.54.0'''
 
-ZIPPED_RELEASE_VERSION = '''0.53.6'''
+ZIPPED_RELEASE_VERSION = '''0.54.0'''
 ZIPPED_RELEASE_LINK_FORMAT = 'https://github.com/Windows81/Roblox-Freedom-Distribution/releases/download/%s/%s.%s.7z'
 
 PLACE_IDEN_CONST = 1818
