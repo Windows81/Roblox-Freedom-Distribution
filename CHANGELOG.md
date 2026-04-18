767c67c3833344ed8bbbf79ff69316d81d5d8276
Windows81 <nastyahmede@gmail.com>
2026-04-18 06:35:51 +0000

    2026-04-18T0636Z

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# 0.65.1
+
+- Added support for modern CSGv3 unions
+- Fixed `--clear_temp_cache` command flag
--- Source/assets/serialisers/mesh/__init__.py
+++ Source/assets/serialisers/mesh/__init__.py
@@ -18,20 +18,22 @@ def parse(original_data: bytes) -> bytes | None:
         mesh_version = rbxmesh.get_mesh_version(original_data)
     except Exception:
         return None
-    match mesh_version:
 
-        # v6/v7: export as v4 if bones present, v2 otherwise; bones are not yet tested.
-        case "6.00" | "6.01" | "7.00" | "7.01":
-            mesh_data = rbxmesh.read_mesh_data(original_data)
-            if len(mesh_data.bones) > 0:
-                return bytes(rbxmesh.export_mesh_v4(mesh_data))
-            else:
-                return bytes(rbxmesh.export_mesh_v2(mesh_data))
+    if mesh_version < "4.01":
+        return original_data
 
-        case "4.01" | "5.00" | "5.01":
-            mesh_data = rbxmesh.read_mesh_data(original_data)
-            return bytes(rbxmesh.export_mesh_v4(mesh_data))
+    '''
+    # Quite simply replaces the header with `version 4.00`.
+    elif mesh_version == "4.01":
+        return b''.join([
+            b'version 4.00',
+            original_data[0xc:],
+        ])
+    '''
 
-        # All other versions: return as-is.
-        case _:
-            return original_data
+    mesh_data = rbxmesh.read_mesh_data(original_data)
+    # Exports as v4 if bones present, v2 otherwise; bones are not yet tested.
+    if len(mesh_data.bones) > 0:
+        return bytes(rbxmesh.export_mesh_v4(mesh_data))
+    else:
+        return bytes(rbxmesh.export_mesh_v2(mesh_data))
